21fb4bb1bd85d2997e86465e2c8586a10f09c87c
evilsmile <freedominmind@163.com>
2016-11-24 10:28:20 +0800

    add watcher test

--- zk_test_c/zk_test.c
+++ zk_test_c/zk_test.c
@@ -4,15 +4,22 @@
 #include <time.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #include <zookeeper/zookeeper.h>
 
 #include "logger.h"
 
 #define BUF_SIZ 512
+#define DISABLE_WATCHER 0
+#define ENABLE_WATCHER 1
 
 static char *username = NULL;
 static char *passwd = NULL;
 static zhandle_t *zh;
+
+static int watcher_notified = 0;
 
 void watcher(zhandle_t *zzh, int type, int state, const char *path, void *watcherCtx) 
 {
@@ -32,15 +39,52 @@ int init_zk()
     return 0;
 }
 
+int clean_node(char *root_path) 
+{
+    struct String_vector children;
+    int i = 0;
+
+    int error = zoo_get_children(zh, root_path, DISABLE_WATCHER, &children);
+    if (error) {
+        log_err("get root node children of '%s' failed: [%d]: %s", root_path, error, zerror(error));
+        return;
+    }
+
+    int root_path_len = strlen(root_path);
+    char wholepath[1024] = {0};
+    for (i = 0; i < children.count; ++i) {
+        memset(wholepath, 0, sizeof(wholepath));
+        memcpy(wholepath, root_path, root_path_len);
+        memcpy(wholepath + root_path_len, "/", 1);
+        memcpy(wholepath + root_path_len + 1, children.data[i], strlen(children.data[i]));
+
+        log_info("Delete [%d]: %s", i, wholepath);
+        error = zoo_delete(zh, wholepath, -1);
+        if (error) {
+            log_err("delete children '%s' failed: [%d]: %s", children.data[i], error, zerror(error));
+            continue;
+        }
+    }
+
+    // clean root node
+    error = zoo_delete(zh, root_path, -1);
+    if (error) {
+        log_err("delete node '%s' create failed: [%d]: %s", root_path, error, zerror(error));
+        return -1;
+    }
+
+    return 0;
+}
+
 void test_unsafe_create_and_get()
 {
+    log_info("test unsafe functions -------------------");
+
 	char buffer[BUF_SIZ];
     int buflen = BUF_SIZ;
 
     struct Stat stat;
     struct String_vector children;
-
-    int i = 0;
 
     struct ACL CREATE_ONLY_ACL_UNSAFE[] = {{ZOO_PERM_CREATE, ZOO_ANYONE_ID_UNSAFE}};
     struct ACL_vector CREATE_ONLY = {1, CREATE_ONLY_ACL_UNSAFE};
@@ -66,7 +110,7 @@ void test_unsafe_create_and_get()
     memset(buffer, 0, sizeof(buffer));
 
     // Get root node value
-    error = zoo_get(zh, test_root_node, 0, buffer, &buflen, &stat);
+    error = zoo_get(zh, test_root_node, DISABLE_WATCHER, buffer, &buflen, &stat);
     if (error) {
         log_err("Get node '%s' failed: [%d]:%s", test_root_node, error, zerror(error));
         goto clean;
@@ -104,7 +148,7 @@ void test_unsafe_create_and_get()
 
     memset(buffer, 0, sizeof(buffer));
 
-    error = zoo_get(zh, test_eph_node_path, 0, buffer, &buflen, &stat);
+    error = zoo_get(zh, test_eph_node_path, DISABLE_WATCHER, buffer, &buflen, &stat);
     if (error) {
         log_err("Get node '%s' failed: [%d]:%s", test_eph_node_path, error, zerror(error));
         goto clean;
@@ -129,37 +173,10 @@ void test_unsafe_create_and_get()
     log_info("sequence node create node [%s] succ", test_seq_node_path);
 
 clean:
-    // clean all children
-    error = zoo_get_children(zh, test_root_node, 0, &children);
-    if (error) {
-        log_err("get root node children of '%s' failed: [%d]: %s", test_eph_node_path, error, zerror(error));
-        return;
-    }
-
-    int test_root_node_len = strlen(test_root_node);
-    char wholepath[1024] = {0};
-    for (i = 0; i < children.count; ++i) {
-        memset(wholepath, 0, sizeof(wholepath));
-        memcpy(wholepath, test_root_node, test_root_node_len);
-        memcpy(wholepath + test_root_node_len, "/", 1);
-        memcpy(wholepath + test_root_node_len + 1, children.data[i], strlen(children.data[i]));
-
-        log_info("Delete [%d]: %s", i, wholepath);
-        error = zoo_delete(zh, wholepath, -1);
-        if (error) {
-            log_err("delete children '%s' failed: [%d]: %s", children.data[i], error, zerror(error));
-            continue;
-        }
-    }
-
-    // clean root node
-    error = zoo_delete(zh, test_root_node, -1);
-    if (error) {
-        log_err("delete node '%s' create failed: [%d]: %s", test_root_node, error, zerror(error));
-        return;
-    }
-
+    clean_node(test_root_node);
 }
+
+
 
 void test_auth_create_and_get()
 {
@@ -168,6 +185,9 @@ void test_auth_create_and_get()
             return;
         }
     }
+
+    log_info("test auth functions -------------------");
+
 	char buffer[BUF_SIZ];
     int buflen = BUF_SIZ;
 
@@ -193,6 +213,158 @@ void test_auth_create_and_get()
     log_info("%s create succ", test_root_node);
 }
 
+enum cmd {
+    SET_ROOT_VALUE = 0,
+    ADD_CHILD = 1
+};
+
+typedef struct _help_args {
+    int cmd;
+
+    zhandle_t *zh;
+    char *path;
+    char *value;
+} help_args;
+
+void handle_set_root_value(zhandle_t* zh, char *root_path, char *new_value)
+{
+    if (zoo_set(zh, root_path, new_value, strlen(new_value), -1)) {
+        log_err("[%s] set to new value failed.", root_path);
+    } else {
+        log_info("[%s] set to new value succ.", root_path);
+    }
+}
+
+void handle_add_child(zhandle_t* zh, char *child_path, char *value)
+{
+    char buffer[512];
+    if (zoo_create(zh, 
+                  child_path, 
+                  value, strlen(value), 
+                  &ZOO_OPEN_ACL_UNSAFE,
+                  0,
+                  buffer, sizeof(buffer) - 1
+                  )) {
+        log_err("create [%s] failed.", child_path);
+    } else {
+        log_info("create [%s] succ.", child_path);
+    }
+}
+
+void* handle_cmd(void *arg)
+{
+    help_args* p_args = (help_args*)arg;
+
+    int cmd = p_args->cmd;
+
+    switch(cmd) {
+    case SET_ROOT_VALUE:
+        handle_set_root_value(p_args->zh, p_args->path, p_args->value);
+        break;
+    case ADD_CHILD:
+        handle_add_child(p_args->zh, p_args->path, p_args->value);
+        break;
+        
+    default:
+        log_err("unknown cmd: %d", cmd);
+        break;
+    }
+
+}
+void request_theother(help_args* args)
+{
+    pthread_t tid;
+    pthread_create(&tid, NULL, handle_cmd, (void*)args);
+    int *res = 0;
+    pthread_join(tid, (void**)&res);
+}
+
+void watcher_root_cb(zhandle_t* zh, int type, int state, const char* path, void *watchCtx)
+{
+    log_info("test root notified by: type:%d state:%d path:%s", type, state, path);
+
+    watcher_notified = 1;
+}
+
+void watcher_child_cb(zhandle_t* zh, int type, int state, const char* path, void *watchCtx)
+{
+    log_info("watcher child notified by: type:%d state:%d path:%s", type, state, path);
+
+    watcher_notified = 1;
+}
+
+void test_watcher()
+{
+    log_info("test watcher functions -------------------");
+
+    char buffer[BUF_SIZ];
+    int buflen = BUF_SIZ;
+    struct String_vector children;
+
+    char *watcher_root = "/test_watcher";
+    char *node_value = "test watcher";
+
+    int error = zoo_create(zh,
+            watcher_root,
+            node_value, strlen(node_value),
+            &ZOO_OPEN_ACL_UNSAFE,
+            0,
+            buffer, sizeof(buffer) - 1
+            );
+    if (error) {
+        log_err("'%s' create failed: [%d]: %s", watcher_root, error, zerror(error));
+    } else {
+        log_info("'%s' create succ", watcher_root);
+    }
+
+    error = zoo_wget(zh, watcher_root, watcher_root_cb, NULL, buffer, &buflen, NULL);
+    if (error) {
+        log_err("'%s' get failed: [%d]: %s", watcher_root, error, zerror(error));
+        goto clean;
+    } else {
+        log_info("'%s' get succ", watcher_root);
+    }                  
+
+    help_args args;
+    args.cmd = SET_ROOT_VALUE;
+    args.zh = zh;
+    args.path = watcher_root;
+    args.value = "root new value";
+    request_theother(&args);
+
+    // wait for theother done
+    sleep(1);
+
+    if (watcher_notified == 1) {
+        log_info("root watcher get notified succ");
+        watcher_notified = 0;
+    } else {
+        log_info("root watcher get notified failed");
+        goto clean;
+    }
+
+    // test child watcher
+    error = zoo_wget_children(zh, watcher_root, watcher_child_cb, NULL, &children);
+
+    args.cmd = ADD_CHILD;
+    args.zh = zh;
+    args.path = "/test_watcher/watcher_child1";
+    args.value = "watcher child 1";
+    request_theother(&args);
+
+    sleep(1);
+ 
+    if (watcher_notified == 1) {
+        log_info("child watcher get notified succ");
+    } else {
+        log_info("child watcher get notified failed");
+        goto clean;
+    }           
+
+clean:
+    clean_node(watcher_root);
+}
+
 void lets_test()
 {
     test_unsafe_create_and_get();
@@ -201,6 +373,7 @@ void lets_test()
         test_auth_create_and_get();
     }
 
+    test_watcher();
 }
 
 void close_zk()
